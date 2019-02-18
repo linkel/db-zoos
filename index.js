8a41052aaf3d13b130de7075c7480e065fa74e00
linkel <ironcarbide@gmail.com>
2019-02-18 09:39:21 -0800

    Bears table and endpoints added.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-// POST
+// ZOOS TABLE
 
 server.post('/api/zoos', (req, res) => {
   const zoo = req.body;
@@ -64,12 +64,87 @@ server.delete('/api/zoos/:id', (req, res) => {
   })
 })
 
-server.put('/api/zoos', (req, res) => {
+server.put('/api/zoos/:id', (req, res) => {
+  const id = req.params.id;
   const zoo = req.body;
   if (!zoo.name) {
     res.status(500).json({error: "Please provide a name."});
   } else {
-    db('zoo').where("id",id).update('name',zoo.name)
+    db('zoos').where("id",id).update('name',zoo.name)
+    .then(updated => {
+      if (updated.length < 1){
+        res.status(404).json({error: `This ID ${id} does not exist!`})
+      } else {
+        res.status(200).json(updated);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({error: "Failed to update DB."});
+    })
+  }
+})
+
+// BEARS TABLE
+
+server.post('/api/bears', (req, res) => {
+  const bear = req.body;
+  if (!bear.name) {
+    res.status(500).json({error: "Please provide a name."});
+  } else {
+    db.insert(bear)
+    .into('bears')
+    .then(ids => {
+      res.status(201).json(ids[0]);
+    })
+    .catch(err => {
+      res.status(500).json({error: "Failed to post to db."});
+    })
+  }
+})
+
+server.get('/api/bears', (req, res) => {
+  db.select().table('bears')
+  .then(response => {
+    res.status(200).json(response)
+  })
+  .catch(err => res.status(500).json({error : "Could not get from db"}))
+})
+
+server.get('/api/bears/:id', (req, res) => {
+  const id = req.params.id;
+  db.select().table('bears').where("id", id)
+  .then(response => {
+    if (response.length < 1) {
+      res.status(404).json({error: "ID does not exist."})
+    } else {
+      res.status(200).json(response)
+    }
+  })
+  .catch(err => res.status(500).json({error : "Could not complete GET from db."}))
+})
+
+server.delete('/api/bears/:id', (req, res) => {
+  const id = req.params.id;
+  db('bears').where("id", id).del()
+  .then(response => {
+    if (response < 1) {
+      res.status(404).json({message : `ID ${id} doesn't exist!`})
+    } else {
+      res.status(200).json({message: `Successfully deleted bear with ID${id}`})
+    }
+  })
+  .catch(err => {
+    res.status(500).json({error: "Could not delete from db"})
+  })
+})
+
+server.put('/api/bears/:id', (req, res) => {
+  const id = req.params.id
+  const bear = req.body;
+  if (!bear.name) {
+    res.status(500).json({error: "Please provide a name."});
+  } else {
+    db('bears').where("id",id).update('name',bear.name)
     .then(updated => {
       if (updated.length < 1){
         res.status(404).json({error: `This ID ${id} does not exist!`})
