4c27e289eab1e56eb6e1e99c905b2af246ae56b1
linkel <ironcarbide@gmail.com>
2019-02-17 18:46:38 -0800

    Added code for POSTing a named zoo to SQLite db.

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+// POST
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  if (!zoo.name) {
+    res.status(500).json({error: "Please provide a name."});
+  } else {
+    db.insert(zoo)
+    .into('zoos')
+    .then(ids => {
+      res.status(201).json(ids[0]);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+  }
+})
 
 const port = 3300;
 server.listen(port, function() {
